f85ef1811525aacc8a08a7c123b897fb3408d192
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-21 23:07:02 +0100

    Refactor rate retrieval method in ZeroCurve class
    Refactor rate interpolation to use numpy's interp function instead of scipy's interp1d.

--- courbe_zero.py
+++ courbe_zero.py
@@ -1,15 +1,19 @@
 import numpy as np
-from scipy.interpolate import interp1d
+
 
 class ZeroCurve:
+
     def __init__(self, tenors, rates):
-        self.tenors=np.array(tenors)
-        self.rates=np.array(rates)
-        self.curve=interp1d(self.tenors,self.rates,kind="linear",fill_value="extrapolate")
 
-    def get_rate(self,maturity):
-        return float(self.curve(maturity))
+        self.tenors = np.array(tenors)
+        self.rates = np.array(rates)
 
-    def discount_factor(self,maturity):
-        r=self.get_rate(maturity)
-        return 1/((1+r)**maturity)
+    def get_rate(self, maturity):
+
+        return float(
+            np.interp(
+                maturity,
+                self.tenors,
+                self.rates
+            )
+        )
