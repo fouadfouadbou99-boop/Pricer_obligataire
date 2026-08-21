b4cfdadab01f5da820b1baf38505712f0686b045
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-21 23:26:05 +0100

    Refactor ZeroCurve to load data from CSV file

--- courbe_zero.py
+++ courbe_zero.py
@@ -1,19 +1,16 @@
+import pandas as pd
 import numpy as np
 
 
 class ZeroCurve:
 
-    def __init__(self, tenors, rates):
+    def __init__(self, fichier):
 
-        self.tenors = np.array(
-            tenors,
-            dtype=float
-        )
+        self.df = pd.read_csv(fichier)
 
-        self.rates = np.array(
-            rates,
-            dtype=float
-        )
+        self.tenors = self.df["tenor"].values
+        self.rates = self.df["rate"].values
+
 
     def get_rate(self, maturity):
 
@@ -23,14 +20,4 @@ class ZeroCurve:
                 self.tenors,
                 self.rates
             )
-        )
-
-    def parallel_shift(
-        self,
-        shift
-    ):
-
-        return ZeroCurve(
-            self.tenors,
-            self.rates + shift
         )
