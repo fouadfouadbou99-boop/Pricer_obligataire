ba204e88548ec34ad78a1b879e82585534b9a403
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-22 14:03:49 +0100

    Refactor Bond class to use coupon_rate instead of coupon

--- bond.py
+++ bond.py
@@ -1,14 +1,53 @@
+from scipy.optimize import newton
+
+
 class Bond:
 
     def __init__(
         self,
         nominal,
-        coupon,
+        coupon_rate,
         maturity,
-        frequency
+        frequency=2
     ):
 
         self.nominal = nominal
-        self.coupon = coupon
+        self.coupon_rate = coupon_rate
         self.maturity = maturity
         self.frequency = frequency
+
+    def cashflows(self):
+
+        flows = []
+
+        n = int(
+            self.maturity *
+            self.frequency
+        )
+
+        coupon = (
+            self.nominal *
+            self.coupon_rate /
+            self.frequency
+        )
+
+        for i in range(1, n + 1):
+
+            t = i / self.frequency
+
+            cf = coupon
+
+            if i == n:
+                cf += self.nominal
+
+            flows.append((t, cf))
+
+        return flows
+
+    def price(self, curve):
+
+        value = 0
+
+        for t, cf in self.cashflows():
+
+            rate = curve.get_
