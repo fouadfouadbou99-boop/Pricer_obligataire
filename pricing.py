becd44cadb4bcd14abba95329c795132b0fa6c1d
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-22 13:09:19 +0100

    Add price_bond function to calculate bond prices
    Implement bond pricing function using cashflows.

---
+++ pricing.py
@@ -0,0 +1,25 @@
+from modules.cashflows import generate_cashflows
+
+
+def price_bond(
+        bond,
+        curve):
+
+    times, cfs = generate_cashflows(
+        bond
+    )
+
+    price = 0
+
+    for t, cf in zip(
+            times,
+            cfs):
+
+        r = curve.get_rate(t)
+
+        price += (
+            cf /
+            ((1 + r) ** t)
+        )
+
+    return price
