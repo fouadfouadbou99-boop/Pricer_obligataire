8d46f358bdf80346343767813f2888b1662b70be
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-22 13:06:33 +0100

    Refactor Bond class constructor and remove cashflows

--- bond.py
+++ bond.py
@@ -1,90 +1,16 @@
-from scipy.optimize import newton
-
-
 class Bond:
 
     def __init__(
         self,
+        code,
         nominal,
-        coupon_rate,
+        coupon,
         maturity,
-        frequency=2
+        frequency
     ):
 
+        self.code = code
         self.nominal = nominal
-        self.coupon_rate = coupon_rate
+        self.coupon = coupon
         self.maturity = maturity
         self.frequency = frequency
-
-    def cashflows(self):
-
-        flows = []
-
-        n = int(
-            self.maturity
-            * self.frequency
-        )
-
-        coupon = (
-            self.nominal
-            * self.coupon_rate
-            / self.frequency
-        )
-
-        for i in range(1, n + 1):
-
-            t = i / self.frequency
-
-            cf = coupon
-
-            if i == n:
-                cf += self.nominal
-
-            flows.append(
-                (t, cf)
-            )
-
-        return flows
-
-    def price(
-        self,
-        curve
-    ):
-
-        value = 0
-
-        for t, cf in self.cashflows():
-
-            rate = curve.get_rate(t)
-
-            value += (
-                cf /
-                ((1 + rate) ** t)
-            )
-
-        return value
-
-    def ytm(
-        self,
-        market_price
-    ):
-
-        def objective(y):
-
-            value = 0
-
-            for t, cf in self.cashflows():
-
-                value += (
-                    cf /
-                    ((1 + y) ** t)
-                )
-
-            return value - market_price
-
-        return float(
-            newton(
-                objective,
-                0.05
-            )
-        )
