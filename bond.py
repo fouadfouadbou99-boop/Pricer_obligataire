c0249e1405844074798ef7eab7058c21f97d8b4b
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-22 13:29:00 +0100

    Remove 'code' parameter from Bond class

--- bond.py
+++ bond.py
@@ -2,14 +2,12 @@ class Bond:
 
     def __init__(
         self,
-        code,
         nominal,
         coupon,
         maturity,
         frequency
     ):
 
-        self.code = code
         self.nominal = nominal
         self.coupon = coupon
         self.maturity = maturity
